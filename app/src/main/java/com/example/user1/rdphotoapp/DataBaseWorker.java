205e23bcda47709c7455112f1d2106347c07cb5f
Capitalsav <capitalsav@gmail.com>
2017-01-15 19:26:20 +0200

    Created class for Data base

--- app/src/main/java/com/example/user1/rdphotoapp/DataBaseWorker.java
+++ app/src/main/java/com/example/user1/rdphotoapp/DataBaseWorker.java
@@ -1,7 +1,11 @@
 package com.example.user1.rdphotoapp;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.content.Context;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by user1 on 15.01.2017.
@@ -9,25 +13,89 @@ import android.database.sqlite.SQLiteDatabase;
 
 public class DataBaseWorker {
 
-    private DBHelper mDbHelper;
+    private final Context mContext;
 
-    public static boolean writeToDb(DBHelper DbHelper, String data) {
-        SQLiteDatabase database = DbHelper.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
+    private DBHelper mDBHelper;
+    private SQLiteDatabase mDataBase;
 
-        contentValues.put(DBHelper.SQL_PHOTO_COLUMN, data);
-
-        long rowID = database.insert(DBHelper.SQL_PHOTOS_TABLE, null, contentValues);
-
-        if (rowID != -1) {
-            return true;
-        }
-        else {
-            return false;
-        }
+    public DataBaseWorker(Context context) {
+        mContext = context;
     }
 
-    public static void readFromDb(DBHelper dbHelper) {
-        
+    /**
+     * open connection to DataBase
+     */
+    public void open() {
+        mDBHelper = new DBHelper(mContext, DBHelper.SQL_DATABASE_NAME,
+                null, DBHelper.SQL_DATABASE_VERSION);
+        mDataBase = mDBHelper.getWritableDatabase();
+    }
+
+    /**
+     * close connection to DataBase
+     */
+    public void close() {
+        if (mDBHelper!=null) mDBHelper.close();
+    }
+
+    /**
+     * get all data from the DataBase
+     * @return Cursor
+     */
+    public Cursor getAllData() {
+        return mDataBase.query(DBHelper.SQL_PHOTOS_TABLE, null, null, null, null, null, null);
+    }
+
+    /**
+     * add record to DataBase Table
+     * @param data
+     */
+    public void writeFromDataBase(String data) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DBHelper.SQL_PHOTO_COLUMN, data);
+        mDataBase.insert(DBHelper.SQL_PHOTOS_TABLE, null, contentValues);
+
+    }
+
+    /**
+     * remove record from DataBase Table
+     * @param id long with photo row id
+     */
+    public void removeFromDataBase(long id) {
+        mDataBase.delete(DBHelper.SQL_PHOTOS_TABLE, DBHelper.SQL_PHOTOS_ID + " = " + id, null);
+    }
+}
+
+
+class DBHelper extends SQLiteOpenHelper {
+
+    public final static int SQL_DATABASE_VERSION = 1;
+
+    public final static String SQL_DATABASE_NAME = "my_db";
+
+    public final static String SQL_PHOTO_COLUMN = "path";
+
+    public final static String SQL_PHOTOS_TABLE = "photos";
+
+    public final static String SQL_PHOTOS_ID = "_id";
+
+    private final static String SQL_CREATE_TABLE = "create table " + SQL_PHOTOS_TABLE + " ( " +
+            SQL_PHOTOS_ID +" integer primary key autoincrement," +
+            SQL_PHOTO_COLUMN + " text);";
+
+    public DBHelper(Context context, String name,
+                    SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.d(MainActivity.TAG, "onCreate database");
+        db.execSQL(SQL_CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
     }
 }
